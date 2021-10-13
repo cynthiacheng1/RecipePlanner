1eddfafbd84602c33ab56e0ad2616a43906461b9
cynthiacheng1 <cc5469@nyu.edu>
2021-10-13 15:40:13 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # RecipePlanner
+
+How To Use:
+clone repository 
+run python recipeplanner.py 
+
+Go to: 
+http://127.0.0.1:5000/pantry
+
+Example Submissions:
+cheese,dough,basil,tomato sauce (will return pizza)
+cheese,basil,tomato sauce (will return spaghetti)
+
+Limitations/Bugs:
+Have only implemented two recipes, all other ingredient combos will lead to No Recipe 
+the order in which you submit ingredients affects which recipe is chosen (will fix)
---
+++ recipeplanner.py
@@ -0,0 +1,42 @@
+from flask import Flask, redirect, url_for, render_template, request 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("base.html", content = "")
+
+
+recipeDic = {"cheese,dough,basil,tomato sauce" : "pizza", "cheese,basil,tomato sauce" : "spaghetti"}
+
+def findRecipe(ingredient_str):
+    if ingredient_str in recipeDic:
+        return recipeDic[ingredient_str]
+    else:
+        return "No Recipe"
+
+
+@app.route("/pantry", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        ingredients = request.form["food"]
+        recipename = findRecipe(ingredients)
+        return redirect(url_for("recipe", recipe=recipename))
+    else:
+        return render_template("pantry.html")
+
+@app.route("/<recipe>")
+def recipe(recipe):
+    return f"<h1> You can make {recipe}! </h1> <h1> {recipe} Directions </h1> <p> Directions </p> <p>Health Info</p>"
+
+
+# @app.route("/<usr>")
+# def user(usr):
+#     return f"<h1> Hi {usr} </h1>"
+
+# @app.route("/admin")
+# def admin():
+#     return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run()
