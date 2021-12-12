4420eb542c6cd10576be7883c681ac2bc5d3ccc3
Ashoka Shringla <ashokashringla@gmail.com>
2021-12-12 18:54:19 -0500

    Update app-routing.module.ts

--- AngularRecipePlanner/recipe-planner/src/app/app-routing.module.ts
+++ AngularRecipePlanner/recipe-planner/src/app/app-routing.module.ts
@@ -3,14 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AddrecipeComponent } from './addrecipe/addrecipe.component';
 import { LoginComponent } from './login/login.component';
+import { PantryComponent } from './pantry/pantry.component';
+import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
 
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'about', component: AboutComponent, pathMatch: 'full' },
   { path: 'register', component: RegisterComponent, pathMatch: 'full' },
-  { path: 'addrecipe', component: AddrecipeComponent, pathMatch: 'full' }
+  { path: 'addrecipe', component: AddrecipeComponent, pathMatch: 'full' },
+  { path: 'pantry', component: PantryComponent, pathMatch: 'full'},
+  { path: 'search', component: SearchComponent, pathMatch: 'full'},
+  { path: 'profile', component: ProfileComponent, pathMatch: 'full'}
 
 
 ];
