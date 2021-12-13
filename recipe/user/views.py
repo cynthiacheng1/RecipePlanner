593b2fc27388515bd842a56d30ac83e3ead91c7e
Raina Kim <hrk296@nyu.edu>
2021-12-13 12:05:38 +0900

    login/register

--- recipe/user/views.py
+++ recipe/user/views.py
@@ -1,3 +1,45 @@
+import json
+import bcrypt
+import jwt
+
+from django.views import View
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import User
+from django.conf import settings
+
+class SignUpView(View):
+    def post(self,request):
+        try:
+            data = json.loads(request.body)
+            password = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt())
+            crypted = password.decode('utf-8')
+            User.objects.create(
+                name = data['name'],
+                email = data['email'],
+                password = crypted
+            )
+            return HttpResponse(status = 200)
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEYS'},status = 400)
+        
+
+class SignInView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+        SECRET_KEY = getattr(settings, "SECRET_KEY", None)
+        try:
+            if User.objects.filter(email = data['email']).exists():
+                user = User.objects.get(email = data['email'])
+                if bcrypt.checkpw(data['password'].encode('utf-8'),user.password.encode('utf-8')):
+                    token = jwt.encode({'email':data['email']}, SECRET_KEY, algorithm = 'HS256')
+                    token = token.decode('utf-8')
+                    return JsonResponse({'token':token}, status = 200)
+                else:
+                    return JsonResponse({'message':'INVALID_USER'}, status = 401)
+            else:
+                return JsonResponse({'message':'INVALID_USER'}, status = 401)
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEYS'}, status = 400)
