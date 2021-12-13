f832e379f4ef7dcda75cddd5527ccdca97628f8c
Raina Kim <hrk296@nyu.edu>
2021-12-13 12:35:17 +0900

    userinfo

--- recipe/user/urls.py
+++ recipe/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView,SignInView
+from .views import SignUpView,SignInView,ProfileView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
+    path('/info', ProfileView.as_view())
 ]
