2725360f324e2034827a210e6f4b4798cb9a07b0
Raina Kim <hrk296@nyu.edu>
2021-12-15 15:03:50 +0900

    fix post

--- recipe/user/urls.py
+++ recipe/user/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
     path('/info', ProfileView.as_view()),
-    path('/addpantry/<str:ingredient>', AddPantryView.as_view()),
+    path('/addpantry', AddPantryView.as_view()),
     path('/pantry', GetPantryView.as_view()),
     path('/pantry-recipe', PantryRecipeView.as_view()),
     path('/addfavorite', FavoriteRecipeView.as_view()),
